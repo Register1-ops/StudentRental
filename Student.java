759a6e686b829c2c2ac3704f1fee73b10d988015
Shaan N <nagarsd1@cardiff.ac.uk>
2025-12-21 15:58:00 +0000

    set up Student file with getters and setters for universityname and studentid

--- Student.java
+++ Student.java
@@ -4,6 +4,28 @@ public class Student extends User{
     private String universityName;
     private String studentId;
     
-    
+    public Student(int id, String name, String email, String passwordHash, String universityName, String studentId)
+        super(id, name, email, passwordHash);
+        this.universityName = universityName;
+        this.studentId = studentId;
+
+    public String getUniversityName(){
+        return universityName;
+    }
+    public void setUniversityName(){
+        this.universityName = universityName;
+    }
+
+    public String getStudentId(){
+        return studentId;
+    }
+    public void setStudentId(){
+        this.studentId = studentId;
+    }
+
+    @Override
+    public String getRole(){
+        return "STUDENT";
+    }
 
 }
