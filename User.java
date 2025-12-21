df4d3aee164d523a90520080262a8c993c73a0ed
Shaan N <nagarsd1@cardiff.ac.uk>
2025-12-21 15:31:55 +0000

    inital Stuent and User java files

---
+++ User.java
@@ -0,0 +1,51 @@
+public abstract class User{
+    protected final int id;
+    protected String name;
+    protected String email;
+    protected String passwordHash;
+    protected boolean active = true;
+
+
+    public User(int id, String name, String email, String passwordHash){
+        this.id =  id;
+        this.name = name;
+        this.email = email;
+        this.passwordHash = passwordHash;
+
+    }
+
+        public int getId(){
+            return id;
+        }
+
+        public booleanisActive(){
+            return active;
+        }
+        public void deactivate(){
+            this.active = false;
+        }
+
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name = name;
+        }
+
+        public String getEmail(){
+            return email;
+        }
+        public void setEmail(String email){
+            this.email = email;
+        }
+
+        public String passwordHash(){
+            return passwordHash;
+        }
+        public void setPasswordHash(){
+            this.passwordHash = passwordHash;
+        }
+
+        public abstract String getRole();
+
+}
