f593f47aeba789d65a742c875268f2797322f286
Shaan N <nagarsd1@cardiff.ac.uk>
2025-12-23 12:25:23 +0000

    added a List of proeprties variable and method to return the properties a homeowner has

--- Homeowner.java
+++ Homeowner.java
@@ -4,13 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Homeowner extends User{
+    private List<Property> properties = new ArrayList<>;
 
     public Homeowner(int id, String name, String email, String passwordHash){
         super(id, name ,email , passwordHash);
     }
 
+    public List<Property> getProperties(){
+        return properties;
+    }
+
+    public void addProperty(Property property){
+        properties.add(property);
+    }
+    public void removeProperty(Property property){
+        properties.remove(property);
+    }
+
+    @Override
     public String getRole(){
         return "HOMEOWNER";
     }
 
 }
+
+// add and remove property , update/view property
