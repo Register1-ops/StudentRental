e713758bc1039a20a2ebdea2e0857d6cf97503f0
Shaan N <nagarsd1@cardiff.ac.uk>
2025-12-27 11:45:41 +0000

    added methods to property and added variables and basics ofthe menu to the student rentals app file

--- Property.java
+++ Property.java
@@ -10,5 +10,48 @@ public class Property{
     private List<Review> reviews = new ArrayList<>();
     private double averageRating 0.0;
 
+    public Property(int id, Homeowner homeowner, String address, String cityOrArea, String description){
+        this.id = id;
+        this.homeowner = homeowner;
+        this.address = address;
+        this.description = description;
+        this.cityOrArea = cityOrArea;
+    }
+
+    public int getId(){
+        return id;
+    }
+    public Homeowner getHomeOwner(){
+        return homeowner;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public String getCityOrArea(){
+        return cityOrArea;
+    }
+    public String getDescription(){
+        return description;
+    }
+
+    public List<Room> getRooms(){
+        return rooms;
+    }
+
+    public List<Review> getReviews(){w
+        return reviews;
+    }
+    public double getAverageRating(){
+        return averageRating;
+    }
+
+    public void addRoom(Room room){
+        rooms.add(room);
+    }
+    public void removeRooms(Room room){
+        rooms.remove(room);
+    }
+
+    public void addReviews(Review reviews)
 
 }
--- StudentRentalsApp.java
+++ StudentRentalsApp.java
@@ -1,3 +1,67 @@
-public static void main(String[][] args){
+package studentrentals;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Scanner;
+
+public class StudentRentalsApp{
     
+    private UserService userService = new UserService();
+    private PropertyService propertyService = new PropertyService();
+    private BookingService bookingService = new BookingService();
+    private ReviewService reviewService = new ReviewService();
+    private SearchService searchService = new SearchService(propertyService);
+
+    private User currentUser;
+    private Scanner scanner = new Scanner(System.in);
+
+    public static void(String[] args){
+        new StudentRentalsApp().run()
+    }
+
+    public void run(){
+        boolean exit = false;
+        while (!exit){
+            if (currentUser == null){
+                exit = showWelcomeMenu();
+            }
+            else{
+                switch (currentUser.getRole()){
+                    case "STUDENT" -> showStudentMenu();
+                    case "HOMEOWNER" -> showHomeOwnerMenu();
+                    case "ADMIN" -> showAdminMenu();
+                }
+                
+            }
+        }
+        System.out.println("Goodbye.")
+
+
+        private boolean showWelcomeMenu(){
+            System.out.println("\n======Student Rentals====");
+            System.out.println(" 1. Register as a Student");
+            System.out.println(" 2. Register as a Home owner");
+            System.out.println(" 3. Login");
+            System.out.println(" 4. Exit");
+            System.out.print("\nChoice  = ");
+            String choice = scanner.nextLine();
+
+            switch choice();
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
